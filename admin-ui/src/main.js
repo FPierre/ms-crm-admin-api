4bfb0676da37fe487a6592abaa578f587efe9a6a
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-06-27 13:23:53 +0200

    Fix store vuex

--- admin-ui/src/main.js
+++ admin-ui/src/main.js
@@ -2,16 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueResource from 'vue-resource'
 import App from './App'
+import store from './store'
 import router from './router'
 
-Vue.use(Vuex)
+Vue.use(VueResource).use(Vuex)
+
+Vue.http.options.root = 'http://localhost:4000'
+// Vue.http.options.xhr = { withCredentials: true }
+Vue.http.options.emulateJSON = true
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }

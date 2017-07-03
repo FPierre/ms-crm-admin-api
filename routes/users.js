14e3c76c6f060040f4ad795d50deb3cfbf5cd552
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-03 15:39:22 +0200

    Ajout de la méthode index

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,10 @@ const router = express.Router()
 
 const requester = new cote.Requester({ name: 'user requester', key: 'user' })
 
-router.get('/', (req, res, next) => res.send('respond with a resource'))
+router.get('', (req, res, next) => {
+  requester.send({ type: 'index' }, (err, users) => res.send(users))
+})
+
 
 router.post('/login', (req, res, next) => {
   requester.send({ type: 'login', user: req.body.user }, user => res.send(user))
